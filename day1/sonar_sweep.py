d1e1bcea2c2f612dd3a4a6b9e14059106f775585
Arne Mertz <arnemertz@users.noreply.github.com>
2021-12-01 09:01:33 +0100

    day 1, part 2

--- day1/sonar_sweep.py
+++ day1/sonar_sweep.py
@@ -11,22 +11,56 @@ def count_depth_increases(depths: Iterable[int]) -> int:
     return depth_increases
 
 
-# test with the example from the task
-def test_count_depth_increases():
-    example_input = [
-        199,
-        200,
-        208,
-        210,
-        200,
-        207,
-        240,
-        269,
-        260,
-        263
-    ]
-    assert count_depth_increases(example_input) == 7
+SLIDING_WINDOW_SIZE: int = 3
+
+
+def sliding_windows(numbers: Iterable[int]) -> Iterable[int]:
+    windows = []
+
+    def add_to_windows(number: int):
+        for i in range(0, len(windows)):
+            windows[i] += number
+        windows.append(number)
+
+    # prime the deque so the following iterations can return a value
+    number_iter = iter(numbers)
+    for _ in range(0, SLIDING_WINDOW_SIZE - 1):
+        add_to_windows(next(number_iter))
+
+    for n in number_iter:
+        add_to_windows(n)
+        yield windows.pop(0)
+
+
+def count_sliding_window_increases(depths: Iterable[int]):
+    return count_depth_increases(sliding_windows(depths))
 
 
 with open('input.txt') as in_file:
-    print(count_depth_increases(int(line.strip()) for line in in_file))
+    print(count_sliding_window_increases(int(line.strip()) for line in in_file))
+
+# test with the example from the task
+EXAMPLE_INPUT = [
+    199,
+    200,
+    208,
+    210,
+    200,
+    207,
+    240,
+    269,
+    260,
+    263
+]
+
+
+def test_count_depth_increases():
+    assert count_depth_increases(EXAMPLE_INPUT) == 7
+
+
+def test_sliding_windows():
+    assert list(sliding_windows(EXAMPLE_INPUT)) == [607, 618, 618, 617, 647, 716, 769, 792]
+
+
+def test_count_sliding_window_increases():
+    assert count_sliding_window_increases(EXAMPLE_INPUT) == 5
