5d20ec315899dd67395697bc65e42e5fe0fd549b
Arne Mertz <arnemertz@users.noreply.github.com>
2021-12-02 07:48:00 +0100

    day 2, part 2

--- day2/dive.py
+++ day2/dive.py
@@ -1,14 +1,19 @@
 from typing import Iterable
 
 
+def parse_line(line: str) -> tuple[str, int]:
+    words: list[str] = line.split(' ')
+    cmd: str = words[0]
+    amount: int = int(words[1])
+    return cmd, amount
+
+
 def calculate_position(lines: Iterable[str]) -> tuple[int, int]:
     horizontal_pos: int
     depth: int
     horizontal_pos, depth = 0, 0
     for line in lines:
-        words: list[str] = line.split(' ')
-        cmd: str = words[0]
-        amount: int = int(words[1])
+        cmd, amount = parse_line(line)
         match cmd:
             case "forward": horizontal_pos += amount
             case "down": depth += amount
@@ -17,8 +22,27 @@ def calculate_position(lines: Iterable[str]) -> tuple[int, int]:
     return horizontal_pos, depth
 
 
+def calculate_position_with_aim(lines: Iterable[str]) -> tuple[int, int]:
+    horizontal_pos: int
+    depth: int
+    aim: int
+    horizontal_pos, depth, aim = 0, 0, 0
+    for line in lines:
+        cmd, amount = parse_line(line)
+        match cmd:
+            case "down":
+                aim += amount
+            case "up":
+                aim -= amount
+            case "forward":
+                horizontal_pos += amount
+                depth += aim*amount
+            case _: assert False, f"unknown command: {cmd}"
+    return horizontal_pos, depth
+
+
 with open('input.txt') as in_file:
-    hp, d = calculate_position(line.strip() for line in in_file)
+    hp, d = calculate_position_with_aim(line.strip() for line in in_file)
     print(hp * d)
 
 
@@ -34,3 +58,7 @@ EXAMPLE_INPUT = [
 
 def test_calculate_position():
     assert calculate_position(EXAMPLE_INPUT) == (15, 10)
+
+
+def test_calculate_position_with_aim():
+    assert calculate_position_with_aim(EXAMPLE_INPUT) == (15, 60)
